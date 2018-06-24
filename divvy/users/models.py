95c9b9aaa2392bf976c3af5ea2a13faefd110e2f
describer <tholsclaw@protonmail.com>
2018-06-24 09:55:39 -0700

    created group variable within User Model

--- divvy/users/models.py
+++ divvy/users/models.py
@@ -8,6 +8,8 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    group = models.ForeignKey
+    # TODO manytomany
 
     def __str__(self):
         return self.username
