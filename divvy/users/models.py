cf88957097e47ac71385402a159eb040b25725e7
describer <tholsclaw@protonmail.com>
2018-06-24 11:39:50 -0700

    created ShareGroup Model

--- divvy/users/models.py
+++ divvy/users/models.py
@@ -8,7 +8,6 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
-    group = models.ForeignKey
     # TODO manytomany
 
     def __str__(self):
@@ -16,3 +15,14 @@ class User(AbstractUser):
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
+
+class ShareGroup(models.Model):
+    name = models.CharField(_("Name of Group"), blank=True, max_length=255)
+    user = models.ManyToManyField(
+        User,
+        related_name="is_member"
+    )
+
+    def __str__(self):
+        return self.name
