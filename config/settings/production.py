a461425a094f1aaf21b235ab8ba94a474aa2ec41
Maddy Ford <mrhf008@gmail.com>
2018-06-29 12:06:57 -0700

    modified production.py to include mailgun info

--- config/settings/production.py
+++ config/settings/production.py
@@ -135,3 +135,10 @@ LOGGING = {
 
 # Your stuff...
 # ------------------------------------------------------------------------------
+INSTALLED_APPS += ['anymail']  # noqa F405
+EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
+# https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
+ANYMAIL = {
+    'MAILGUN_API_KEY': env('MAILGUN_API_KEY'),
+    'MAILGUN_SENDER_DOMAIN': env('MAILGUN_DOMAIN')
+}
